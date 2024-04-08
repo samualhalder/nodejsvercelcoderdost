dff4c4e5f70aa5acfb496e5b7ffb9cd98064fe2a
samualhalder <samual.halder2020@gmail.com>
2024-04-08 20:48:06 +0530

    added express server

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-const http = require("http");
+const express = require("express");
 const fs = require("fs");
 const index = fs.readFileSync("index.html", "utf-8");
 const data = fs.readFileSync("data.json", "utf-8");
-const server = http.createServer((req, res) => {
-  switch (req.url) {
-    case "/":
-      res.setHeader("content-Type", "text/html");
-      res.end(index);
-      break;
-    case "/data":
-      res.setHeader("content-Type", "application/json");
-      res.end(data);
-      break;
-    default:
-      res.writeHead(404);
-      res.end();
-      break;
-  }
+const morgan = require("morgan");
+
+const server = express();
+// MIDDLEWARES
+server.use(express.json()); //build in middleware -> body perser
+server.use(morgan("dev"));
+
+server.use((req, res, next) => {
+  console.log(req.ip);
+  next();
 });
+
+const auth = (req, res, next) => {
+  console.log(req.body);
+  if (req.body.password == "123") {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+//API-END POINTS
+server.get("/", (req, res) => {
+  res.json(data);
+});
+
+server.post("/", auth, (req, res) => {
+  res.json({ type: "post" });
+});
+server.put("/", (req, res) => {
+  res.json({ type: "put" });
+});
+server.patch("/", (req, res) => {
+  res.json({ type: "patch" });
+});
+server.delete("/", (req, res) => {
+  res.json({ type: "delete" });
+});
+
 server.listen(8080, () => {
   console.log("sercer is started!!!");
 });
